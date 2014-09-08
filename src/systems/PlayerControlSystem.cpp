58b01fd3135c19a6d144a9653e0772a930203e90
Reicher <robin.o.reicher@gmail.com>
2014-09-08 21:53:19 +0200

    added more movement

--- src/systems/PlayerControlSystem.cpp
+++ src/systems/PlayerControlSystem.cpp
@@ -4,6 +4,9 @@
 #include "components/Motion.h"
 #include "KeyHandler.h"
 
+// TBR
+#include "iostream"
+
 PlayerControlSystem::PlayerControlSystem()
 : m_node()
 { 
@@ -19,6 +22,7 @@ void PlayerControlSystem::update(const KeyHandler& keyHandler)
    auto pPlayerControl = std::get<0>(m_node);
    auto pMotion = std::get<1>(m_node);
 
+   // Left and right movement
    if (keyHandler.isPressed(SDLK_a))
    {
       pMotion->velocity.x() = -0.3f;
@@ -30,5 +34,25 @@ void PlayerControlSystem::update(const KeyHandler& keyHandler)
    else
    {
        pMotion->velocity.x() = 0.0f;
+   }
+
+   // Up and down movement
+   if (keyHandler.isPressed(SDLK_w))
+   {
+      pMotion->velocity.y() = 0.3f;
+   }
+   else if (keyHandler.isPressed(SDLK_s))
+   {
+      pMotion->velocity.y() = -0.3f;
+   }
+   else
+   {
+       pMotion->velocity.y() = 0.0f;
+   }
+
+   // Fire
+   if (keyHandler.isPressed(SDLK_SPACE))
+   {
+        std::cout << "Pew pew!" << std::endl;
    }
 }
